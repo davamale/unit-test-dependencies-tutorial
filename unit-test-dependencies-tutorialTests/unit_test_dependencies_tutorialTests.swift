f52e3553e5f9a93d43746f19a9474784c7fcb785
David <davamale@gmail.com>
2018-01-01 21:30:58 -0500

    Initial Commit

---
+++ unit-test-dependencies-tutorialTests/unit_test_dependencies_tutorialTests.swift
@@ -0,0 +1,36 @@
+//
+//  unit_test_dependencies_tutorialTests.swift
+//  unit-test-dependencies-tutorialTests
+//
+//  Created by David Martinez-Lebron on 1/1/18.
+//  Copyright © 2018 David Martinez-Lebron. All rights reserved.
+//
+
+import XCTest
+@testable import unit_test_dependencies_tutorial
+
+class unit_test_dependencies_tutorialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
