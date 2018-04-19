46c6b5133e1d461ff26cc53a5634de6a32898764
David <davamale@gmail.com>
2018-04-19 17:29:46 -0400

    setting up stub services

--- unit-test-dependencies-tutorialTests/Stubs/ServiceStub.swift
+++ unit-test-dependencies-tutorialTests/Stubs/ServiceStub.swift
@@ -10,7 +10,35 @@ import Foundation
 import MapKit
 import Contacts
 
+struct DummyData {
+    static let streetAddres = "269–299 Geary St"
+    static let city = "San Francisco"
+    static let subLocality = "Union Square"
+    static let state = "CA"
+    static let postalCode = "94102"
+    static let country = "Estados Unidos"
+    static let isoCountry = "US"
+}
+
 class ServiceStub: LocationServiceType, ApiClientType {
+    
+    var addressDictionary: [String: Any] {
+        let addresDict =
+            [CNPostalAddressStreetKey : DummyData.streetAddres,
+             CNPostalAddressCityKey : DummyData.city,
+             CNPostalAddressSubLocalityKey: DummyData.subLocality,
+             CNPostalAddressStateKey : DummyData.state,
+             CNPostalAddressPostalCodeKey : DummyData.postalCode,
+             CNPostalAddressCountryKey : DummyData.country,
+             CNPostalAddressISOCountryCodeKey : DummyData.isoCountry]
+        return addresDict
+    }
+    
+    func currentAddress(completion: @escaping (MKPlacemark?) -> ()) {
+        let placemarkd = MKPlacemark(coordinate: CLLocationCoordinate2D(), addressDictionary: addressDictionary)
+        completion(placemark)
+    }
+    
     func currentAddress(completion: @escaping (MKPlacemark?) -> ()) {
         
     }
